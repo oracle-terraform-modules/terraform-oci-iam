07232fc284ff48a262d11b91be7de4b7df9fd642
Yiming Wu <yiming.wu@oracle.com>
2019-01-17 17:18:34 -0800

    Update description variable.

--- modules/iam-user/variables.tf
+++ modules/iam-user/variables.tf
@@ -8,8 +8,10 @@ variable "user_name" {
   description = "The name you assign to the user during creation. The name must be unique across all compartments in the tenancy. "
 }
 
+// The description is only used if user_create = true.
 variable "user_description" {
   description = "The description you assign to the user. Does not have to be unique, and it's changeable. "
+  default     = " "
 }
 
 variable "user_create" {
