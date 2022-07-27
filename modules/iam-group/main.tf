3aa4d438f905fcb8e805b48b1cfb7b4cbe88bca0
Andrea Marchesini <andrea.marchesini@oracle.com>
2022-07-27 11:14:53 +0000

    required_provider updated to use oracle/oci

--- modules/iam-group/main.tf
+++ modules/iam-group/main.tf
@@ -1,13 +1,4 @@
-// Copyright (c) 2018, 2021, Oracle and/or its affiliates.
-
-terraform {
-  required_version = ">= 0.12" // terraform version below 0.12 is not tested/supported with this module
-  required_providers {
-    oci = {
-      version = ">= 3.27" // force downloading oci-provider compatible with terraform v0.12
-    }
-  }
-}
+// Copyright (c) 2018, 2022, Oracle and/or its affiliates.
 
 ########################
 # Group
