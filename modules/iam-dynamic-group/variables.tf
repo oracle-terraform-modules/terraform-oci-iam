54edc58f4e3321bda613e53500b9e34ae51bf81f
Abdullah Talakey <abdullah.talakey@gmail.com>
2023-03-16 18:01:58 +0300

    upgrade to support terraform version > 0.12

--- modules/iam-dynamic-group/variables.tf
+++ modules/iam-dynamic-group/variables.tf
@@ -1,55 +1,55 @@
 // Copyright (c) 2018, 2021, Oracle and/or its affiliates.
 
 variable "tenancy_ocid" {
-  type = string
+  type        = string
   description = "The OCID of the tenancy."
-  default = null
+  default     = null
 }
 
 variable "dynamic_group_name" {
-  type = string
+  type        = string
   description = "The name you assign to the group during creation. The name must be unique across all compartments in the tenancy. "
-  default = null
+  default     = null
 }
 
 variable "dynamic_group_description" {
-  type = string
+  type        = string
   description = "The description you assign to the Group. Does not have to be unique, and it's changeable. "
   default     = null
 }
 
 variable "dynamic_group_create" {
-  type = bool
+  type        = bool
   description = "(Deprecated) Create the dynamic group or not. If true, the user must have permissions to create the group; If false, group data will be returned about the group if it exists, if not found, then an empty string will be returned for the group ID."
   default     = true
 }
 
 variable "matching_rule" {
-  type = string
+  type        = string
   description = "Define a matching rule or a set of matching rules to define the group members."
   default     = null
 }
 
 variable "policy_name" {
-  type = string
+  type        = string
   description = "The name you assign to the policy during creation.  "
   default     = null
 }
 
 variable "policy_description" {
-  type = string
+  type        = string
   description = "The description you assign to the policy. Does not have to be unique, and it's changeable. "
   default     = null
 }
 
 variable "policy_statements" {
-  type = list(string)
+  type        = list(string)
   description = "Define policy consists of one or more policy statements. "
   default     = null
 }
 
 variable "policy_compartment_id" {
-  type = string
+  type        = string
   description = "The compartment id assign to policy."
   default     = null
 }
